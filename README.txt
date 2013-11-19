fa5311ecbfa9aa1e533cfe3892e5b8be341f472d
porlin72 <porlin72@yahoo.com>
2013-11-18 19:19:12 -0500

    Refactor home page view to use a template

---
+++ README.txt
@@ -0,0 +1,2 @@
+The following is practice for Test Driven Web Development using the following source:
+http://chimera.labs.oreilly.com/books/1234000000754/pr01.html
--- lists/tests.py
+++ lists/tests.py
@@ -8,19 +8,24 @@ Replace this with more appropriate tests for your application.
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-
+        
 class HomePageTest(TestCase):
     
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+        print "Test 1 is working properly."
         
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.endswith(b'</html>'))
+        print "Test 2 is working properly."

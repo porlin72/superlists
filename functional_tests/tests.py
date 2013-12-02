3d9a7fbe01c25a164e1c73ab95a3688fdedc619f
porlin72 <porlin72@yahoo.com>
2013-12-02 18:18:59 -0500

    moved functional_tests to functional_tests app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,15 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+# import unittest
 
-class NewVisitorTest(unittest.TestCase): #1
+class NewVisitorTest(LiveServerTestCase): 
     
-    def setUp(self): #2
+    def setUp(self): 
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
     
-    def tearDown(self): #3
+    def tearDown(self): 
         self.browser.quit()
         
     def check_for_row_in_list_table(self, row_text):
@@ -16,21 +17,22 @@ class NewVisitorTest(unittest.TestCase): #1
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
         
-    def test_can_start_a_list_and_retrieve_it_later(self): #4
+    def test_can_start_a_list_and_retrieve_it_later(self): 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # She notices that page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title) #5
+        self.assertIn('To-Do', self.browser.title) 
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
         
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -40,8 +42,11 @@ class NewVisitorTest(unittest.TestCase): #1
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        
-        import time
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_list_table('1: Buy peacock feathers')
+       
+        # import time
         # time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -62,13 +67,34 @@ class NewVisitorTest(unittest.TestCase): #1
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-        # She visits that URL - her to-do list is still there.
-
+        # Now a new user, Francis, come along to the site.
+        self.browser.quit()
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+        self.browser = webdriver.Firefox()
+        
+        # Francis visits the home page. There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
         # Satisfied, she goes back to sleep
         
-if __name__ == '__main__': #7
-    unittest.main() #8
--- superlists/settings.py
+++ superlists/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'lists',
+    'functional_tests',
 )
 
 # A sample logging configuration. The only tangible logging
